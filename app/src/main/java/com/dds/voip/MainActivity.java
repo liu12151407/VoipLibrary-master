c4ed2ab3fa85d038c4e447af9ac09bf714e20013
LIUYINGLONG <635536177@qq.com>
2020-02-09 21:35:05 +0800

    代码备份

--- app/src/main/java/com/dds/voip/MainActivity.java
+++ app/src/main/java/com/dds/voip/MainActivity.java
@@ -1,18 +1,13 @@
 package com.dds.voip;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.trustmobi.voip.R;
@@ -34,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Button bt_server;
     private Button bt_call;
     private Button bt_login;
+    private TextView tv_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         bt_server = findViewById(R.id.bt_server);
         bt_call = findViewById(R.id.bt_call);
         bt_login = findViewById(R.id.bt_login);
+        tv_info = findViewById(R.id.tv_info);
         bt_server.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,6 +60,41 @@ public class MainActivity extends AppCompatActivity {
                 SipHelper.getInstance().call();
             }
         });
+        countDown();
+    }
+
+    /**
+     * 倒计时
+     */
+    private CountDownTimer timer;
+
+    /**
+     * 倒计时显示
+     */
+    private void countDown() {
+        timer = new CountDownTimer(4000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                String crTime = millisUntilFinished / 1000 + "";
+                switch (crTime) {
+                    case "3":
+                        SipHelper.getInstance().startService();
+                        break;
+                    case "1":
+                        SipHelper.getInstance().login();
+                        break;
+                    default:
+                        break;
+                }
+                tv_info.setText("服务器连接中..." + crTime);
+            }
+
+            @Override
+            public void onFinish() {
+                tv_info.setText("服务器连接成功");
+                SipHelper.getInstance().call();
+            }
+        }.start();
     }
 
     @Override
@@ -77,15 +109,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         if (VoipUtil.isIncall(this)) {
-            ToastUtils.showShort("通话中...");
+            ToastUtils.showShort("通话中");
         } else {
-            ToastUtils.showShort("通话结束...");
+            ToastUtils.showShort("通话结束");
+            tv_info.setText("正在上传数据...");
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        SipHelper.getInstance().close();
+        try {
+            SipHelper.getInstance().close();
+            if (timer != null) {
+                timer.cancel();
+                timer = null;
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            //do something.
+            return true;
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
     }
 }
--- app/src/main/java/com/dds/voip/SipHelper.java
+++ app/src/main/java/com/dds/voip/SipHelper.java
@@ -3,6 +3,7 @@ package com.dds.voip;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.CountDownTimer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
@@ -149,4 +150,5 @@ public class SipHelper {
     public void call() {
         VoipUtil.outgoing(getActivity(), getOtherNumber(), false);
     }
+
 }
